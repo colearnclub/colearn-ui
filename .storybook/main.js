2bc1c0b538cf210857ef445c3fc224bf68c50f45
Slava Sobolev <sobolevslava@gmail.com>
2022-02-08 09:18:13 +0300

    setup storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../packages/**/*.stories.mdx',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
   ],
   framework: '@storybook/react',
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,27 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { appTheme } from '../packages/theme/src';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  chakra: {
+    // theme: appTheme,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-  }
-}
+  },
+};
+
+const theme = extendTheme(appTheme);
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ChakraProvider theme={theme} resetCSS>
+        <Story />
+      </ChakraProvider>
+    );
+  },
+];
