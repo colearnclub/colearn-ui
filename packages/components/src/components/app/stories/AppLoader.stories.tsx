44bbd5cfd1b1b1d77367e0c1825943da6b4b10b9
Slava Sobolev <sobolevslava@gmail.com>
2022-02-09 15:23:44 +0300

    setup storybook

---
+++ packages/components/src/components/app/stories/AppLoader.stories.tsx
@@ -0,0 +1,10 @@
+import AppLoader from '../AppLoader';
+
+export default {
+  title: 'Pages/AppLoader',
+  component: AppLoader,
+};
+
+export const Index = {
+  args: {},
+};
---
+++ packages/components/src/components/app/stories/NotFound.stories.tsx
@@ -0,0 +1,10 @@
+import NotFoundPage from '../NotFoundPage';
+
+export default {
+  title: 'Pages/NotFoundPage',
+  component: NotFoundPage,
+};
+
+export const Index = {
+  args: {},
+};
